97a1f7ad436a172fda4f8a182fcd23a2a9aa3504
shruti-mayank <shrutimayank1@gmail.com>
2023-03-25 18:53:51 +0530

    Nav Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import "./App.css"
+import HomePage from "./pages/HomePage.jsx"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+			<div className="App">
+				<Routes>          
+					<Route path="/" component={HomePage} exact />
+				</Routes>
+			</div>	
+		</BrowserRouter>
   );
 }
-
-export default App;
