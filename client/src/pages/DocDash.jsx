bbf693cddafb90422833fc75780d45e5ecd5dc9c
Shivangibohara <nikki.shiv2402@gmail.com>
2023-03-26 08:03:07 +0530

    Dash Page added

---
+++ client/src/pages/DocDash.jsx
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Menu from "./components/Menu";
+import Container from "./components/Container";
+
+function DocDash() {
+  return (
+    <div className="App">
+      <Menu />
+      <Container />
+    </div>
+  );
+}
+
+export default DocDash;
