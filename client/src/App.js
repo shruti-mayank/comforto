f9918310baf8ddaf8fd106f6f213724887d9d879
DISHA842 <82803738+DISHA842@users.noreply.github.com>
2023-03-26 08:10:00 +0530

    add blog pages

--- client/src/App.js
+++ client/src/App.js
@@ -14,12 +14,40 @@ import { signOut } from "firebase/auth";
 import { auth } from "./firebase-config";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/login";
+    });
+  };
+  
   return (
+    
     <div className="App">
+	   <Router>
+      <nav>
+        <Link to="/blog"> Home </Link>
+
+        {!isAuth ? (
+          <Link to="/login"> Login </Link>
+        ) : (
+          <>
+            <Link to="/createpost"> Create Post </Link>
+            <button onClick={signUserOut}> Log Out</button>
+          </>
+        )}
+      </nav>
       <BrowserRouter>
 				<Routes>          
 					<Route path="/" element={<HomePage />} />
           <Route path="/talk" element={<Specialist />} />
+         <Route path="/blog" element={<Home isAuth={isAuth} />} />
+        <Route path="/createpost" element={<CreatePost isAuth={isAuth} />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
+        <Route path="/form" element={<Form />} />
 				</Routes>
 		  </BrowserRouter>
     </div>
